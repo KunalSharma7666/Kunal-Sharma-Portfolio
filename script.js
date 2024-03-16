9eb7bca96b563416a81986f10f89792d5c860276
Kunal Sharma <108820509+KunalSharma7666@users.noreply.github.com>
2024-03-16 22:24:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// For Auto type on home page
+var typed=new Typed(".Auto-Type",{
+    strings:["","I'm a Developer."," I'm a Coder.","I'm a Freelancer.","I'm a Designer.","I'm a Gamer."],
+    showCursor: false,
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
