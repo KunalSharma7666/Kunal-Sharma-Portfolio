51e9687e57e622c8230b73fca8d3f7e0cc84f102
Kunal Sharma <108820509+KunalSharma7666@users.noreply.github.com>
2024-04-02 20:21:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,40 @@ var typed = new Typed(".Auto-Type",{
     loop:true
 });
 
+
+document.body.addEventListener("keydown" , (ev) => {
+  
+    if(ev.key == "ArrowRight"){
+      $("#flipbook").turn("next");
+    }
+
+    if(ev.key == "ArrowLeft"){
+      $("#flipbook").turn("previous");
+    }
+  })
+
+
+// Get all elements with the class "image-cover"
+const imageCovers = document.querySelectorAll('.image-cover');
+
+// Iterate over each image-cover element
+imageCovers.forEach(imageCover => {
+    // Add event listeners for mouseenter and mouseleave events
+    imageCover.addEventListener('mouseenter', function() {
+        // Change opacity of img and p elements when mouse enters image-cover
+        const img = this.querySelector('img');
+        const p = this.querySelector('p');
+        img.style.opacity = 0.3;
+        p.style.opacity = 1;
+    });
+
+    imageCover.addEventListener('mouseleave', function() {
+        // Reset opacity when mouse leaves image-cover
+        const img = this.querySelector('img');
+        const p = this.querySelector('p');
+        img.style.opacity = 1;
+        p.style.opacity = 0;
+    });
+});
+
+
